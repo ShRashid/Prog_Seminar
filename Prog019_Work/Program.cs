209771924e76214a59744cf2c92d8293ee433188
Rashid <rashid_74@mail.ru>
2023-09-30 14:02:06 +0300

    Добавлены 6 программ

---
+++ Prog019_Work/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача №17. Напишите программу, которая принимает на вход координаты точки (X и Y), 
+// причем X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+Console.WriteLine("Введите число");
+int X = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число");
+int Y = Convert.ToInt32(Console.ReadLine());
+
+if(X>0 && Y>0)
+{
+    Console.WriteLine("1");
+}
+else if(X<0 && Y>0)
+{
+     Console.WriteLine("2");
+}
+else if(X<0 && Y<0)
+{
+     Console.WriteLine("3");
+}
+else
+{
+    Console.WriteLine("4");
+}
---
+++ Prog022_Home/Program.cs
@@ -0,0 +1,52 @@
+﻿// Внутри класса Answer напишите метод IsPalindrome, который принимает на вход пятизначное число number и проверяет, является ли оно палиндромом.
+// Метод должен проверить является ли число пятизначным, в противном случае - вывести Число не пятизначное и False в следующей строке.
+// Для остальных чисел вернуть True или False.
+// 14212 -> False
+// 12821 -> True
+// 234322 -> Число не пятизначное
+//                      False
+using System;
+
+public class Answer
+{
+    static bool IsPalindrome(int number){
+      // Введите свое решение ниже
+    string numberString = number.ToString();  
+    bool isPalindrome = true; 
+
+    if (numberString.Length != 5 ) 
+    {
+      System.Console.WriteLine("Число не пятизначное");
+      isPalindrome = false;  
+      return isPalindrome;
+    }
+ 
+    for (int i = 0; i < numberString.Length / 2; i++)  
+    {  
+        if (numberString[i] != numberString[numberString.Length - 1 - i])  
+        {  
+            isPalindrome = false;  
+            break;  
+        }  
+    }  
+    return isPalindrome; 
+ 
+    }
+  
+  // Не удаляйте и не меняйте метод Main! 
+      static public void Main(string[] args) {
+        int number;
+
+        if (args.Length >= 1) {
+            number = int.Parse(args[0]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            //number = 64546;
+            number = 645464;
+        }
+
+        // Не удаляйте строки ниже
+        bool result = IsPalindrome(number);
+        System.Console.WriteLine($"{result}");
+    }
+}
